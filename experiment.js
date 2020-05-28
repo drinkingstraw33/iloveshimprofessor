b1f485eb8f7d59ae9dbc177dd48d17691258199c
drinkingstraw33 <60614681+drinkingstraw33@users.noreply.github.com>
2020-05-28 09:57:54 +0900

    Add files via upload

--- experiment.js
+++ experiment.js
@@ -1,13 +1,11 @@
-document.onkeydown = function (e) {
-    key = (e) ? e.keyCode : event.keyCode;
-    ctrl = (e) ? e.ctrlKey : event.ctrlKey;
-
-    if ((ctrl == true && (key == 78 || key == 82)) || key == 8 || key == 116) {
-        if (e) {
-            e.preventDefault();
-        } else {
-            event.keyCode = 0;
-            event.returnValue = false;
-        }
+function noEvent() { // 새로 고침 방지
+    if (event.keyCode == 116) {
+        alert("새로고침을 할 수 없습니다.");
+        event.keyCode = 2;
+        return false;
+    } else if (event.ctrlKey &&
+        (event.keyCode == 78 || event.keyCode == 82)) {
+        return false;
     }
 }
+document.onkeydown = noEvent;
